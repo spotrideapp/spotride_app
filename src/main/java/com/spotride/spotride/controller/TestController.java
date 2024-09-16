fa3b53ef96c784333b3308f3dde7bc6ac4709885
N0TARGET <prost1k@yandex.ru>
2024-09-16 22:25:36 +0200

    [SR-11] Setup prometheus + grafana. (#11)
    Co-authored-by: Aleksey Popok <alexeypopok.pl@gmail.com>

--- src/main/java/com/spotride/spotride/controller/TestController.java
+++ src/main/java/com/spotride/spotride/controller/TestController.java
@@ -8,10 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public final class TestController {
+
     private static final String TEST_RESPONSE = "test controller response";
 
     @GetMapping("/test")
     public String test() {
         return TEST_RESPONSE;
     }
+
 }

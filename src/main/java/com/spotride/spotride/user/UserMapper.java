7bacfb4492ea5db34593fdafaec7b3b05ac01182
Andrey Shipulya <andrey.shipulya@vizor-games.com>
2024-10-02 17:31:04 +0200

    add mapper ignore

--- src/main/java/com/spotride/spotride/user/UserMapper.java
+++ src/main/java/com/spotride/spotride/user/UserMapper.java
@@ -5,6 +5,7 @@ import com.spotride.spotride.user.dto.UserResponseDto;
 import com.spotride.spotride.user.dto.request.UserUpdateRequestDto;
 import com.spotride.spotride.user.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 /**
@@ -13,11 +14,18 @@ import org.mapstruct.MappingTarget;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "modifiedAt", ignore = true)
     User toEntity(UserCreateRequestDto userCreateRequestDto);
 
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "modifiedAt", ignore = true)
     User toEntity(UserUpdateRequestDto userUpdateRequestDto);
 
     UserResponseDto toDto(User user);
 
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "modifiedAt", ignore = true)
     void updateEntityFromDto(UserUpdateRequestDto userUpdateRequestDto, @MappingTarget User user);
 }

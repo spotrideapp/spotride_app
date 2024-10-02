eadd43dd583611ca0cddd7a3e88d0e9f2edb879e
Andrey Shipulya <andrey.shipulya@vizor-games.com>
2024-10-02 19:27:31 +0200

    add spring configuration for tests

--- src/test/java/com/spotride/spotride/user/controller/UserControllerTest.java
+++ src/test/java/com/spotride/spotride/user/controller/UserControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.Objects;
@@ -22,6 +23,7 @@ import static org.mockito.Mockito.when;
 /**
  * Tests for {@link UserController}.
  */
+@ActiveProfiles("test")
 @SpringBootTest
 class UserControllerTest {
 
--- src/test/java/com/spotride/spotride/user/service/UserServiceTest.java
+++ src/test/java/com/spotride/spotride/user/service/UserServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -27,6 +28,7 @@ import static org.mockito.Mockito.when;
 /**
  * Tests for {@link UserService}.
  */
+@ActiveProfiles("test")
 @SpringBootTest
 class UserServiceTest {
 
